32c41f8f3dc980a89373897621814eb76d6f1dc6
Majid Kargar <52089654+fulcain@users.noreply.github.com>
2025-06-09 14:29:29 +0330

    cui: Add shadcn

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  tailwindFunctions: ["clsx", "cn"],
+};
