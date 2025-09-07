bf7d6ebe694b52025e42418afc33f3074b43e03c
Majid Kargar <majidkargar01@gmail.com>
2025-09-07 09:32:38 +0330

    refactor: change quick links for BLS

--- app/configs/quickLinks/bls.ts
+++ app/configs/quickLinks/bls.ts
@@ -12,16 +12,16 @@ export const BLS = {
         url: "https://gov.eclipse-rp.net/viewtopic.php?t=90093",
       },
       {
-        name: "Handbook Formats",
-        url: "https://gov.eclipse-rp.net/viewtopic.php?t=90093#FORMATS",
-      },
-      {
         name: "Command Handbook",
         url: "https://gov.eclipse-rp.net/viewtopic.php?p=1055095#p1055095",
       },
       {
         name: "Command Area",
         url: "https://gov.eclipse-rp.net/viewforum.php?f=1948",
+	  },
+      {
+        name: "Discussion Board",
+        url: "https://gov.eclipse-rp.net/viewforum.php?f=1889",
 	  },
       {
         name: "Incoming Applications",
