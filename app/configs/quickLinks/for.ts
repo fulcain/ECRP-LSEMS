bacdac89467cd9b5ed9edd8d9942ac127b5f89a8
Majid Kargar <majidkargar01@gmail.com>
2025-09-25 22:38:28 +0330

    feat: Add a new role for For and add new ranks

--- app/configs/quickLinks/for.ts
+++ app/configs/quickLinks/for.ts
@@ -6,11 +6,20 @@ export const FOR = {
       {
         name: "Division Index",
         url: "https://gov.eclipse-rp.net/viewforum.php?f=2568",
-      },
+			},
       {
         name: "Handbook",
         url: "https://gov.eclipse-rp.net/viewtopic.php?t=164012",
+			},
+      {
+        name: "Serologist Training Guide",
+        url: "https://gov.eclipse-rp.net/viewtopic.php?t=112252",
       },
+      {
+        name: "Student Profiles",
+        url: "https://gov.eclipse-rp.net/viewforum.php?f=1959",
+      },
+
       {
         name: "Reports",
         url: "https://gov.eclipse-rp.net/viewforum.php?f=2577",
