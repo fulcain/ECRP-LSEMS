d50dab996f1d43c7348b2f2e70477c6f2df170c2
Majid Kargar <majidkargar01@gmail.com>
2025-10-13 18:38:56 +0330

    refactor: change general image

--- app/configs/general.ts
+++ app/configs/general.ts
@@ -3,7 +3,7 @@ export const General = {
   image: "/General.png",
   data: {
     imageSize: "150,150",
-    image: "Los Santos Emergency Medical Services",
+    image: "https://i.imgur.com/OA8dOlP.png",
     divisionName: "Los Santos Emergency Medical Services",
     ranks: "",
   },
