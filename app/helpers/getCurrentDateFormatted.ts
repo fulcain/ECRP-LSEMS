ece5cb8e549fed1e7c667cde9566d59f892426a6
Majid Kargar <majidkargar01@gmail.com>
2026-01-11 06:42:39 +0330

    refactor: change the dates to be in full format

--- app/helpers/getCurrentDateFormatted.ts
+++ app/helpers/getCurrentDateFormatted.ts
@@ -2,20 +2,30 @@ export const getCurrentDateFormatted = (): string => {
   const now = new Date();
   const day = now.getUTCDate().toString().padStart(2, "0");
   const monthNames = [
-    "JAN",
-    "FEB",
-    "MAR",
-    "APR",
-    "MAY",
-    "JUN",
-    "JUL",
-    "AUG",
-    "SEP",
-    "OCT",
-    "NOV",
-    "DEC",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ];
   const month = monthNames[now.getUTCMonth()];
   const year = now.getUTCFullYear();
-  return `${day}/${month}/${year}`;
+
+  let daySuffix = "th";
+  if (day === "1") {
+    daySuffix = "st";
+  } else if (day === "2") {
+    daySuffix = "nd";
+  } else if (day === "3") {
+    daySuffix = "rd";
+  }
+
+  return `${month} ${day}${daySuffix}, ${year}`;
 };
